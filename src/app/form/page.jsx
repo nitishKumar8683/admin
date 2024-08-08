468bc3482af3a6abd8681b3f08e81fecb21c43b8
kumarnitish075 <nitishkumar868308@gmail.com>
2024-08-08 14:21:37 +0530

    mobile

--- src/app/form/page.jsx
+++ src/app/form/page.jsx
@@ -1,15 +1,10 @@
 "use client";
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ClipLoader from "react-spinners/ClipLoader";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import { format } from "date-fns";
-import { CalendarIcon } from "@heroicons/react/24/outline";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
 
 const FormLayout = () => {
   const [formData, setFormData] = useState({
@@ -23,16 +18,13 @@ const FormLayout = () => {
   });
   const [errors, setErrors] = useState({});
   const [loading, setLoading] = useState(false);
-  const [dob, setDob] = useState(null); // For react-datepicker
-  const datePickerRef = useRef(null);
-  const [date, setDate] = useState(new Date());
 
   const validateForm = () => {
     const newErrors = {};
     if (!formData.childName) newErrors.childName = "Child's name is required";
     if (!formData.guardianName)
       newErrors.guardianName = "Guardian's name is required";
-    if (!dob) newErrors.dob = "Date of birth is required";
+    if (!formData.dob) newErrors.dob = "Date of birth is required";
     if (!formData.phoneNumber)
       newErrors.phoneNumber = "Phone number is required";
     if (!formData.address) newErrors.address = "Address is required";
@@ -49,7 +41,7 @@ const FormLayout = () => {
     setLoading(true);
     const formattedValues = {
       ...formData,
-      dob: dob ? format(dob, "yyyy-MM-dd") : "", 
+      dob: format(new Date(formData.dob), "yyyy-MM-dd"), 
     };
 
     try {
@@ -68,7 +60,6 @@ const FormLayout = () => {
           dob: "",
           timeOut: "",
         });
-        setDob(null); 
       } else {
         toast.error(response.data.message);
       }
@@ -110,84 +101,18 @@ const FormLayout = () => {
               />
             </div>
 
-            <div className="mb-4 w-full">
+            <div className="mb-4">
               <label className="mb-3 block text-sm font-medium text-black dark:text-white">
                 Date of Birth (DOB)
               </label>
-              <div className="relative w-full">
-                <DatePicker
-                  ref={datePickerRef}
-                  selected={dob}
-                  onChange={(date) => {
-                    setDob(date);
-                    setFormData({ ...formData, dob: date });
-                  }}
-                  dateFormat="yyyy-MM-dd"
-                  placeholderText="Select date"
-                  customInput={
-                    <input
-                      type="text"
-                      className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 pr-10 text-black outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                      readOnly
-                    />
-                  }
-                />
-                <div
-                  className="absolute right-3 top-1/2 -translate-y-1/2 transform cursor-pointer"
-                  onClick={() => datePickerRef.current?.setOpen(true)} 
-                >
-                  <CalendarIcon className="text-gray-400 dark:text-gray-500 h-5 w-5" />
-                </div>
-              </div>
-            </div>
-
-            <div className="mb-4 flex gap-4">
-              <div className="w-1/2">
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Time In
-                </label>
-                <input
-                  type="time"
-                  id="timeIn"
-                  name="timeIn"
-                  placeholder="HH:MM"
-                  value={formData.timeIn}
-                  onChange={(e) =>
-                    setFormData({ ...formData, timeIn: e.target.value })
-                  }
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-              <div className="w-1/2">
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Time Out
-                </label>
-                <input
-                  type="time"
-                  id="timeOut"
-                  name="timeOut"
-                  placeholder="HH:MM"
-                  value={formData.timeOut}
-                  onChange={(e) =>
-                    setFormData({ ...formData, timeOut: e.target.value })
-                  }
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-            </div>
-
-            <div className="mb-4">
-              <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                Address
-              </label>
               <input
-                type="text"
-                id="address"
-                name="address"
-                placeholder="Enter address"
-                value={formData.address}
+                type="date"
+                id="dob"
+                name="dob"
+                placeholder="mm/dd/yyyy"
+                value={formData.dob}
                 onChange={(e) =>
-                  setFormData({ ...formData, address: e.target.value })
+                  setFormData({ ...formData, dob: e.target.value })
                 }
                 className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
               />
@@ -227,7 +152,57 @@ const FormLayout = () => {
               />
             </div>
 
-            <div className="flex justify-center">
+            <div className="mb-4">
+              <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                Address
+              </label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                placeholder="Enter address"
+                value={formData.address}
+                onChange={(e) =>
+                  setFormData({ ...formData, address: e.target.value })
+                }
+                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+              />
+            </div>
+
+            <div className="mb-4 flex gap-4">
+              <div className="w-1/2">
+                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                  Time In
+                </label>
+                <input
+                  type="time"
+                  id="timeIn"
+                  name="timeIn"
+                  value={formData.timeIn}
+                  onChange={(e) =>
+                    setFormData({ ...formData, timeIn: e.target.value })
+                  }
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                />
+              </div>
+              <div className="w-1/2">
+                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                  Time Out
+                </label>
+                <input
+                  type="time"
+                  id="timeOut"
+                  name="timeOut"
+                  value={formData.timeOut}
+                  onChange={(e) =>
+                    setFormData({ ...formData, timeOut: e.target.value })
+                  }
+                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                />
+              </div>
+            </div>
+
+            <div className="flex justify-end">
               <button
                 type="submit"
                 className="hover:bg-primary-dark rounded bg-primary px-4 py-2 text-white transition"
