ad96fcc0a7f34f7e803a0fcd7739f3db90a0efe9
kumarnitish075 <nitishkumar868308@gmail.com>
2024-08-08 10:43:20 +0530

    mobile

--- src/app/form/page.jsx
+++ src/app/form/page.jsx
@@ -57,185 +57,184 @@ const FormLayout = () => {
   return (
     <>
       <ToastContainer />
-<div className="mx-auto mt-8 max-w-lg">
-  <h2 className="text-gray-800 mb-4 text-center text-2xl font-bold">
-    Consent Form
-  </h2>
-  <div className="mb-4 rounded bg-white px-8 pb-8 pt-6 shadow-md">
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-    >
-      {({ values, handleChange, handleBlur }) => (
-        <Form>
-          <div className="mb-4">
-            <label
-              htmlFor="childName"
-              className="text-gray-700 mb-2 block text-sm font-bold"
-            >
-              Child&apos;s Name
-            </label>
-            <Field
-              type="text"
-              id="childName"
-              name="childName"
-              placeholder="Enter child's name"
-              className="form-input border-gray-300 mt-1 block w-full rounded-md px-3 py-2 shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
-            />
-            <ErrorMessage
-              name="childName"
-              component="p"
-              className="text-xs italic text-red"
-            />
-          </div>
+      <div className="mx-auto mt-8 max-w-lg">
+        <h2 className="text-gray-800 mb-4 text-center text-2xl font-bold">
+          Consent Form
+        </h2>
+        <div className="mb-4 rounded bg-white px-8 pb-8 pt-6 shadow-md">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ values, handleChange, handleBlur }) => (
+              <Form>
+                <div className="mb-4">
+                  <label
+                    htmlFor="childName"
+                    className="text-gray-700 mb-2 block text-sm font-bold"
+                  >
+                    Child&apos;s Name
+                  </label>
+                  <Field
+                    type="text"
+                    id="childName"
+                    name="childName"
+                    placeholder="Enter child's name"
+                    className="form-input border-gray-300 mt-1 block w-full rounded-md px-3 py-2 shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
+                  />
+                  <ErrorMessage
+                    name="childName"
+                    component="p"
+                    className="text-xs italic text-red"
+                  />
+                </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="guardianName"
-              className="text-gray-700 mb-2 block text-sm font-bold"
-            >
-              Parent/Guardian&apos;s Name
-            </label>
-            <Field
-              type="text"
-              id="guardianName"
-              name="guardianName"
-              placeholder="Enter parent/guardian's name"
-              className="form-input border-gray-300 mt-1 block w-full rounded-md px-3 py-2 shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
-            />
-            <ErrorMessage
-              name="guardianName"
-              component="p"
-              className="text-xs italic text-red"
-            />
-          </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="guardianName"
+                    className="text-gray-700 mb-2 block text-sm font-bold"
+                  >
+                    Parent/Guardian&apos;s Name
+                  </label>
+                  <Field
+                    type="text"
+                    id="guardianName"
+                    name="guardianName"
+                    placeholder="Enter parent/guardian's name"
+                    className="form-input border-gray-300 mt-1 block w-full rounded-md px-3 py-2 shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
+                  />
+                  <ErrorMessage
+                    name="guardianName"
+                    component="p"
+                    className="text-xs italic text-red"
+                  />
+                </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="dob"
-              className="text-gray-700 mb-2 block text-sm font-bold"
-            >
-              Date of Birth (DOB)
-            </label>
-            <Field
-              type="date"
-              id="dob"
-              name="dob"
-              className="border-gray-300 w-full rounded-md border px-3 py-3 shadow-sm text-lg focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
-            />
-            <ErrorMessage
-              name="dob"
-              component="p"
-              className="text-red mt-1 text-xs italic"
-            />
-          </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="dob"
+                    className="text-gray-700 mb-2 block text-sm font-bold"
+                  >
+                    Date of Birth (DOB)
+                  </label>
+                  <Field
+                    type="date"
+                    id="dob"
+                    name="dob"
+                    className="border-gray-300 w-full rounded-md border px-3 py-3 text-lg shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
+                  />
+                  <ErrorMessage
+                    name="dob"
+                    component="p"
+                    className="mt-1 text-xs italic text-red"
+                  />
+                </div>
 
-          <div className="grid grid-cols-2 gap-4">
-            <div className="mb-4">
-              <label
-                htmlFor="timeIn"
-                className="text-gray-700 mb-2 block text-sm font-bold"
-              >
-                Time In
-              </label>
-              <Field
-                type="time"
-                id="timeIn"
-                name="timeIn"
-                title="Enter time in HH:MM format"
-                className="border-gray-300 w-full rounded-md border px-3 py-3 shadow-sm text-lg focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
-              />
-              <ErrorMessage
-                name="timeIn"
-                component="p"
-                className="text-xs italic text-red"
-              />
-            </div>
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="custom-mobile-input mb-4">
+                    <label
+                      htmlFor="timeIn"
+                      className="text-gray-700 mb-2 block text-sm font-bold"
+                    >
+                      Time In
+                    </label>
+                    <Field
+                      type="time"
+                      id="timeIn"
+                      name="timeIn"
+                      title="Enter time in HH:MM format"
+                      className="border-gray-300 w-full rounded-md border px-3 py-3 text-lg shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
+                    />
+                    <ErrorMessage
+                      name="timeIn"
+                      component="p"
+                      className="text-xs italic text-red"
+                    />
+                  </div>
 
-            <div className="mb-4">
-              <label
-                htmlFor="timeOut"
-                className="text-gray-700 mb-2 block text-sm font-bold"
-              >
-                Time Out
-              </label>
-              <Field
-                type="time"
-                id="timeOut"
-                name="timeOut"
-                placeholder="Select Time Out"
-                className="border-gray-300 w-full rounded-md border px-3 py-3 shadow-sm text-lg focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
-              />
-              <ErrorMessage
-                name="timeOut"
-                component="p"
-                className="text-xs italic text-red"
-              />
-            </div>
-          </div>
+                  <div className="custom-mobile-input mb-4">
+                    <label
+                      htmlFor="timeOut"
+                      className="text-gray-700 mb-2 block text-sm font-bold"
+                    >
+                      Time Out
+                    </label>
+                    <Field
+                      type="time"
+                      id="timeOut"
+                      name="timeOut"
+                      placeholder="Select Time Out"
+                      className="border-gray-300 w-full rounded-md border px-3 py-3 text-lg shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
+                    />
+                    <ErrorMessage
+                      name="timeOut"
+                      component="p"
+                      className="text-xs italic text-red"
+                    />
+                  </div>
+                </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="phoneNumber"
-              className="text-gray-700 mb-2 block text-sm font-bold"
-            >
-              Phone Number
-            </label>
-            <Field
-              type="tel"
-              id="phoneNumber"
-              name="phoneNumber"
-              placeholder="Enter phone number"
-              className="form-input border-gray-300 mt-1 block w-full rounded-md px-3 py-2 shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
-            />
-            <ErrorMessage
-              name="phoneNumber"
-              component="p"
-              className="text-xs italic text-red"
-            />
-          </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="phoneNumber"
+                    className="text-gray-700 mb-2 block text-sm font-bold"
+                  >
+                    Phone Number
+                  </label>
+                  <Field
+                    type="tel"
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    placeholder="Enter phone number"
+                    className="form-input border-gray-300 mt-1 block w-full rounded-md px-3 py-2 shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
+                  />
+                  <ErrorMessage
+                    name="phoneNumber"
+                    component="p"
+                    className="text-xs italic text-red"
+                  />
+                </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="address"
-              className="text-gray-700 mb-2 block text-sm font-bold"
-            >
-              Address
-            </label>
-            <Field
-              as="textarea"
-              id="address"
-              name="address"
-              placeholder="Enter address"
-              rows={3}
-              className="form-textarea border-gray-300 mt-1 block w-full rounded-md px-3 py-2 shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
-            />
-            <ErrorMessage
-              name="address"
-              component="p"
-              className="text-xs italic text-red"
-            />
-          </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="address"
+                    className="text-gray-700 mb-2 block text-sm font-bold"
+                  >
+                    Address
+                  </label>
+                  <Field
+                    as="textarea"
+                    id="address"
+                    name="address"
+                    placeholder="Enter address"
+                    rows={3}
+                    className="form-textarea border-gray-300 mt-1 block w-full rounded-md px-3 py-2 shadow-sm focus:border-primary focus:ring focus:ring-primary focus:ring-opacity-50"
+                  />
+                  <ErrorMessage
+                    name="address"
+                    component="p"
+                    className="text-xs italic text-red"
+                  />
+                </div>
 
-          <div className="flex justify-center">
-            {loading ? (
-              <ClipLoader color="#4a90e2" loading={loading} size={35} />
-            ) : (
-              <button
-                type="submit"
-                className="rounded-md bg-primary px-4 py-2 text-white shadow-md hover:bg-opacity-90 focus:outline-none focus:ring-2 focus:ring-primary"
-              >
-                Submit
-              </button>
+                <div className="flex justify-center">
+                  {loading ? (
+                    <ClipLoader color="#4a90e2" loading={loading} size={35} />
+                  ) : (
+                    <button
+                      type="submit"
+                      className="rounded-md bg-primary px-4 py-2 text-white shadow-md hover:bg-opacity-90 focus:outline-none focus:ring-2 focus:ring-primary"
+                    >
+                      Submit
+                    </button>
+                  )}
+                </div>
+              </Form>
             )}
-          </div>
-        </Form>
-      )}
-    </Formik>
-  </div>
-</div>
-
+          </Formik>
+        </div>
+      </div>
     </>
   );
 };
